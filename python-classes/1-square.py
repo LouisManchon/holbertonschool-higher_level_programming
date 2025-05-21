66b0bac15417028d4a07f1d8737b8067d7a8726d
LouisManchon <10578@holbertonstudents.com>
2025-05-21 18:00:17 +0200

    Instantiation with size (no type/value verification)

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This module Allow to learn POO with class
+"""
+
 
 class Square:
+    """
+    Define a class named Square
+    """
     def __init__(self, size):
         self.__size = size
