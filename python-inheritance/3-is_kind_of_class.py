22a18f9ee3b5059b7981765975540acea8e7baf5
LouisManchon <10578@holbertonstudents.com>
2025-05-28 11:23:51 +0200

    fix space

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,14 +2,18 @@
 """
 check if the object is an instance of a class or class that inherited from
 """
+
+
 def is_kind_of_class(obj, a_class):
     """
     the obj and the class
     """
+
     if isinstance(obj, a_class):
         """
         check if True or False
         """
+
         return True
     else:
         return False
