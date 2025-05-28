13d868918fa04b1b8567ee33f1943f1355543841
LouisManchon <10578@holbertonstudents.com>
2025-05-28 16:49:55 +0200

    update task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module is just a class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Initializes a square
+    """
+
+    def __init__(self, size):
+        """
+        Init square validator
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Define area
+        """
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__width, self.__height))
