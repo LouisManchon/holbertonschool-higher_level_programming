a5693717d9920bd474f4d41af5c51c81fc90052b
LouisManchon <10578@holbertonstudents.com>
2025-05-28 11:20:46 +0200

    fix space

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+function that returns True if the object isan instance of the class
+"""
+
 
 def is_same_class(obj, a_class):
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
