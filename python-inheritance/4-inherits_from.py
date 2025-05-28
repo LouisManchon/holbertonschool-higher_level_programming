54921f2461c8abb788e96edbfde71bf7a83a5f60
LouisManchon <10578@holbertonstudents.com>
2025-05-28 11:24:59 +0200

    fix space

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,14 +2,18 @@
 """
 check if the object is an instance of a class that inherited (directly or indirectly) from the specified class
 """
+
+
 def inherits_from(obj, a_class):
     """
     the object and the class
     """
+
     if issubclass(obj,(a_class)) and type(obj) != a_class:
         """
         condition that check if True or False
         """
+
         return True
     else:
         return False
