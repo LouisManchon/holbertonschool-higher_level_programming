481c2fe3c175be4097ba73afd71041c6395e62ca
LouisManchon <10578@holbertonstudents.com>
2025-05-28 15:01:33 +0200

    fix space

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,18 +2,22 @@
 """
 This module is just a class
 """
+
+
 class BaseGeometry:
     """
     This is the BaseGeometry class
     """
+
     def area(self):
         """
         Raises an Exception indicating that area is not implemented
         """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-         """
+        """
         Validates that value is a positive integer.
 
         Args:
@@ -24,7 +28,8 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is less than or equal to 0.
         """
-        if not isinstance(value, int):
+
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
