e3e9fb00ece7f5c289a1e3799054b30efc14924c
LouisManchon <10578@holbertonstudents.com>
2025-05-29 17:44:11 +0200

    fix docstring and space

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,17 +1,43 @@
+#!/usr/bin/python3
 from abc import ABC, abstractmethod
 
+"""
+This module have abstract class and subclass
+"""
+
+
 class Animal(ABC):
+    """
+    Abstract class allow subclasses generique methodes
+    """
 
     @abstractmethod
     def sound(self):
+        """
+        Methods for all subclass
+        """
         pass
 
 
 class Dog(Animal):
+    """
+    Class of dog
+    """
+
     def sound(self):
-        print(f"Bark")
+        """
+        Sound of the dogs
+        """
+        return "Bark"
 
 
 class Cat(Animal):
+    """
+    Class of cat
+    """
+
     def sound(self):
-        print(f"Meow")
+        """
+        Sound of the cats
+        """
+        return "Meow"
