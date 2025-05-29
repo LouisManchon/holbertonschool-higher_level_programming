e5b74dde8fd26cacd2f58bf2234b67e7a7834f1a
LouisManchon <10578@holbertonstudents.com>
2025-05-29 17:13:05 +0200

    Create an abstract class with 2 subclass

---
+++ python-inheritance/python-abc/task_00_abc.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        print(f"Bark")
+
+
+class Cat(Animal):
+    def sound(self):
+        print(f"Meow")
