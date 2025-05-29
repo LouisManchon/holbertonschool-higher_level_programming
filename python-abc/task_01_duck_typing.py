2841c12f17858a517fcf40a10615096e709291e3
LouisManchon <10578@holbertonstudents.com>
2025-05-29 19:18:08 +0200

    fix space

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,85 +1,58 @@
 #!/usr/bin/python3
 """
-This module use duck typing concept to create subclasses and adapted methods
+This module uses duck typing to create subclasses and adapted methods
 """
-
 
 from abc import ABC, abstractmethod
 import math
 
-"""
-This module have abstract class and subclass
-"""
-
 
 class Shape(ABC):
     """
-    Abstract class allow subclasses generic methods
+    Abstract class defining a generic shape
     """
 
     @abstractmethod
     def area(self):
-        """
-        Methods for all subclass
-        """
         pass
 
     @abstractmethod
     def perimeter(self):
-        """
-        Methods for all subclass
-        """
         pass
 
 
 class Circle(Shape):
     """
-    Class of Circle
+    Circle shape class
     """
 
     def __init__(self, radius):
-        """
-        Initialize the circle
-        """
-        self.__radius = radius
+        self.radius = radius
 
     def area(self):
-        """
-        Define area of the circle
-        """
-        return math.pi * self.radius * self.radius
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
-        """
-        Define perimeter of the circle
-        """
         return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
     """
-    Class of rectangle
+    Rectangle shape class
     """
 
     def __init__(self, width, height):
-        """
-        Initialize the rectangle
-        """
         self.width = width
         self.height = height
 
     def area(self):
-        """
-        Define area of the rectangle
-        """
         return self.width * self.height
 
     def perimeter(self):
-        """
-        Define area of the rectangle
-        """
         return 2 * (self.width + self.height)
 
-    def shape_info(shape):
-        print("Area:", shape.area())
-        print("Perimeter:", shape.perimeter())
+
+# ✅ Cette fonction doit être ici, EN DEHORS des classes
+def shape_info(shape):
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
