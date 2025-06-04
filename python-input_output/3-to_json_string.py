9aa810ab2cdd83ea38d55a0116512dd4e537d9dc
LouisManchon <10578@holbertonstudents.com>
2025-06-04 19:02:43 +0200

    Update function return  the JSON representation of an object (string)

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module allow the function
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object (string)
+    """
+    json.my_obj = json.dumps(my_obj)
+    return my_obj
