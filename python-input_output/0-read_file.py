cffc110404e463249c030c72df760177fc4a0008
LouisManchon <10578@holbertonstudents.com>
2025-06-04 18:07:19 +0200

    Fix docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module allow a reading files
+"""
+
+
 def read_file(filename=""):
+    """
+    Funtion to read a file
+    """
     with open (filename, 'r', encoding='utf-8') as f:
         print(f.read(), end="")
