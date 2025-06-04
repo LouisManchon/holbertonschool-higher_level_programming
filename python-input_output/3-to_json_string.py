d280502b8bfd3cea59bf766daa789463beaa1ec6
LouisManchon <10578@holbertonstudents.com>
2025-06-04 19:05:44 +0200

    fix f.read

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -9,6 +9,6 @@ def to_json_string(my_obj):
     """
     function that returns the JSON representation of an object (string)
     """
-    with open(f.write, 'w', endcoding='utf-8') as f:
+    with open(f.read, 'r', endcoding='utf-8') as f:
         json.my_obj = json.dumps(my_obj)
     return my_obj
