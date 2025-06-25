161bfa37ac226915990d73ed08609530202bd61d
LouisManchon <10578@holbertonstudents.com>
2025-06-25 12:23:00 +0200

    fix  comments

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- Create a table `second_table` in the database `hbtn_0c_0` in the MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
