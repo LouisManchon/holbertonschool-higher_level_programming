ea23b26edcfdd9f0b060e9ac99ff5a6886d2cbe5
LouisManchon <10578@holbertonstudents.com>
2025-06-25 12:39:30 +0200

    Fix condition

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,7 +1,4 @@
 -- Updates the score of Bob to 10 in the table `second_table`.
 UPDATE second_table
-JOIN (
-    SELECT score FROM second_table WHERE name = 'John'
-) AS temp
-SET second_table.score = temp.score
-WHERE second_table.name = 'Bob';
+SET score = 10
+WHERE name = 'Bob';
