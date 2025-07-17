3700f263f759109a28724a10310c32f928e9564c
LouisManchon <10578@holbertonstudents.com>
2025-07-17 11:57:40 +0200

    fix space

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,9 +1,6 @@
-document.querySelector('#toggle_header').addEventListener('click'), function () {
+document.querySelector('#toggle_header').addEventListener('click', function () {
     const header = document.querySelector('header');
 
-    if (header.classList.contains('red')) {
-        header.classList.replace('red', 'green');
-    } else {
-        header.classList.replace('green')
-    }
-};
+    header.classList.toggle('red');
+    header.classList.toggle('green');
+});
