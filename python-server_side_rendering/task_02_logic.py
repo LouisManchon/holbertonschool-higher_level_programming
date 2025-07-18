8f3150d1618c78d7ee4d6bfaec43b1325921822f
LouisManchon <10578@holbertonstudents.com>
2025-07-18 23:08:02 +0200

    Update task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -17,12 +17,10 @@ def contact():
 
 @app.route('/items')
 def items():
-    try:
-        with open('items.json', 'r', encoding='utf-8') as i:
-            datas = json.load(i)
-        return render_template('items.html', items=datas.get("items"))
-    except Exception as e:
-        print({"error": "{}".format(e)})
+    with open('items.json') as f:
+        data = json.load(f)
+    items_list = data.get("items", [])
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
